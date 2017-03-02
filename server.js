57485cf634b8a0ebfe2a9d1dbe053b7de29fc14a
Sam Lill <ssl@ksu.edu>
2017-03-02 04:22:31 -0600

    -Server up and working for multiple clients

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 var express = require('express');
-
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var players = [];
+var games = 0;
+const Game = require('./server/game');
+
+// Server starts listening on port 5000
+server.listen('5000', ()=>{
+    console.log('Listening at http://localhost:5000');
+});
 
 app.use(express.static('public'));
-
-app.listen('5000', ()=>{
-  'Listening at http://localhost:5000';
+app.get('/', function (req, res, next) {
+    res.sendFile(__dirname + '/index.html');
 });
+
+app.use('/static', express.static('node_modules'));
+
+// Handles a player connection
+io.on('connection', function (socket) {
+    console.log('A user connected');
+    players.push(socket);
+
+    // If we have two players, Launch a game instance
+    if (players.length == 2) {
+        new Game(io, players, games);
+        players = [];
+        games++;
+    }
+});
+
+
