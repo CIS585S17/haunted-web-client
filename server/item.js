af3d37adfcae5a67254ea44cdee504dce716c90b
laurenkl96 <lklynch@ksu.edu>
2017-03-09 17:04:58 -0600

    added item skeleton

--- server/item.js
+++ server/item.js
@@ -2,17 +2,29 @@
 
 module.exports = exports = Item;
 
-function Item(socket) {
-    this.id = 'item';
-    this.socket = socket;
+function Item(id) {
+    this.id = id;
+    this.name = "";
+    this.desc = "";
+    this.used = false;
 
     this.send = {
         id: this.id
     };
+    this.setNameDesc(id);
 }
 
 Item.prototype.update = function () {
     this.send = {
         id: this.id
     };
+}
+
+Item.prototype.setNameDesc = function (id) {
+    switch(id){
+        case 0: 
+            this.name = "Sword";
+            this.desc = "ajksdlf;ahwfeewkmfnks;danp";
+        break;
+    }
 }
