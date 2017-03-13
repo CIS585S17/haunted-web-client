260a885a8d0930b404f2a5b91149a88d852cc58e
Nasser Alhamadah Alhamadah <Nasser Alhamadah Alhamadah>
2017-03-12 19:17:46 -0500

    added items control in the character class

--- UI test/character.js
+++ UI test/character.js
@@ -11,6 +11,9 @@ function Character ()
 	this.inventory;
 	this.messagesLog;
 	
+	// might be moved outside of this class
+	this.itemsPointer;
+	
 }
 
 Character.prototype.setUp = function  (name, portraitImage, maxHp, power, speed, sanity)
@@ -30,6 +33,7 @@ Character.prototype.setUp = function  (name, portraitImage, maxHp, power, speed,
 	this.sanity = sanity;
 	this.inventory = [];
 	this.messagesLog = [];
+	this.itemsPointer = 0;
 	
 }
 
@@ -57,14 +61,72 @@ Character.prototype.changeSanity = function(amount)
 	this.sanity+=amount;
 }
 
-Character.prototype.addItem = function(item)
+Character.prototype.addItem = function(item)// place of the item in the inventory list
 {
 	this.inventory.push(item);
 	
+}
+
+Character.prototype.removeItem = function(item)// place of the item in the inventory list
+{
+	this.inventory.splice(item,1);
+	
+	
+	// this part should be removed if we don't use a scrollable item bar
+	if (item < this.itemsPointer)
+	{
+		itemsPointer--;
+	}
+	else 
+	{
+		if (item< this.itemsPointer+2) // if it is removing an item before the half of the viewed items
+		{
+			this.itemsPointer--;
+		}
+		
+		else 
+		{
+			if (!(this.itemsPointer<this.inventory.length-4))
+			{
+				this.itemsPointer--;
+			}
+			
+			else
+			{
+				//Do nothing	
+			}
+			
+		}
+		
+	}
+	
+	if (this.itemsPointer<0)
+	{
+		this.itemsPointer = 0;
+	}
 }
 
 Character.prototype.addMessage = function(message)
 {
 	this.messagesLog.push(message);
 	
+}
+
+Character.prototype.scrollItemsRight = function()
+{
+	if (this.inventory.length>4) // assumming that we are viewing 4 items 
+	{
+		if (this.itemsPointer<this.inventory.length-4) // you can only move if the items being viewed will not go byoned the last postion  
+		{
+			this.itemsPointer++;
+		}
+	}
+}
+
+Character.prototype.scrollItemsLeft = function()
+{
+	if (this.inventory.length>0)
+	{
+		this.itemsPointer--;
+	}
 }
