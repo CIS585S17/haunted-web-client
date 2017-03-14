dfbded303f43171778c94b8c6aad9f6a35746903
laurenkl96 <lklynch@ksu.edu>
2017-03-14 16:55:05 -0500

    skeleton event.js

--- server/event.js
+++ server/event.js
@@ -2,17 +2,27 @@
 
 module.exports = exports = Event;
 
-function Event(socket) {
-    this.id = 'event';
-    this.socket = socket;
+function Event(id, player) {
+    this.id = id;
+    this.playerInvolved = player;
+    this.cinematic;
 
     this.send = {
         id: this.id
     };
+    this.setStatChanges(this.playerInvolved, this.id);
 }
 
 Event.prototype.update = function () {
     this.send = {
         id: this.id
     };
+}
+
+Event.prototype.setStatChanges = function(player, id){
+    switch(id){
+      case "0":
+        player.health --;
+      break;
+    }
 }
