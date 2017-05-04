eba40b8228455d22f1dc35ddd74b1ec55db27f4f
Gunnarr07 <jeremyrt88@gmail.com>
2017-05-04 17:12:31 -0500

    work on character display

--- renderer/game_queue/game-queue.js
+++ renderer/game_queue/game-queue.js
@@ -1,6 +1,20 @@
 'use strict'
 const {ipcRenderer} = require('electron')
+const $ = require('jquery')
 
-ipcRenderer.on('load', (event, options) => {
-  console.log(options)
+ipcRenderer.on('load', (event, data) => {
+  console.log(data)
+  let chars = data.options.characters
+  for (let i = 0; i < chars.length; i++) {
+    if (i === 0) {
+      for (let item in chars[i]) {
+        $('#character_0').append(`<li class="list-group-item">${item.toString()}</li>`)
+      }
+    }
+    if (i === 1) {
+      for (let item in chars[i]) {
+        $('#character_0').append(`<li class="list-group-item">${item.toString()}</li>`)
+      }
+    }
+  }
 })
