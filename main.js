ba76c3958abca2bc37fdc7872d467c3445468c57
Gunnarr07 <jeremyrt88@gmail.com>
2017-05-12 14:01:01 -0500

    refactor

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const {WindowGraph} = require('./main/windows')
 const socket = require('socket.io-client')('https://haunted-server.herokuapp.com')
 // const socket = require('socket.io-client')('http://localhost:5000')
 
-let debug = true
+let debug = false
 let game = new Game()
 let windowGraph = new WindowGraph(BrowserWindow, debug, __dirname)
 let queueWindow
