5b8e1359b34a30235523e6c9d482264f12d1ed0d
Gunnarr07 <jeremyrt88@gmail.com>
2017-05-11 19:49:36 -0500

    merge changes

---
+++ renderer/Item.js
@@ -0,0 +1,63 @@
+class Item {
+	constructor(scene, x, y, z, rx, ry, rz, sx, sy, sz) {
+		this.radians = Math.PI/180;
+		this.model = this.LoadModel(scene, x, y, z, rx, ry, rz, sx, sy ,sz);
+		this.vec = {
+			x:Math.cos(ry),
+			y:0,
+			z:Math.sin(ry)
+		};
+	}
+
+	LoadModel(scene, x, y, z, rx, ry, rz, sx, sy, sz) {
+		var geometry = new THREE.BoxGeometry(sx, sy, sz);
+			for ( var i = 0; i < geometry.faces.length; i ++ ) {
+    		geometry.faces[ i ].color.setHex( Math.random() * 0xffffff );
+			}
+		var material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } )
+		var box = new THREE.Mesh(geometry, material);
+		box.position.set(x, y, z);
+		box.rotation.set(rx, ry, rz);
+		scene.add(box);
+		return box;
+	}
+
+	GetModel() {
+	  return this.model;
+	}
+
+	GetVector() {
+		return this.vec;
+	}
+
+	GetPos() {
+		let p = {
+			x: this.model.position.x,
+			y: this.model.position.y,
+			z: this.model.position.z
+		}
+		return {
+			position: p
+		};
+	}
+
+	GetCollisionModel() {
+		let modelWidth = 0.25;
+		let modelHeight = 0.25;
+		let modelDepth = 0.25;
+		let min_X = this.model.position.x - modelWidth/2;
+		let max_X = this.model.position.x + modelWidth/2;
+		let min_Y = this.model.position.y - modelHeight/2;
+		let max_Y = this.model.position.y + modelHeight/2;
+		let min_Z = this.model.position.z - modelDepth/2;
+		let max_Z = this.model.position.z + modelDepth/2;
+		return{	minX: min_X, maxX: max_X,
+					 	minY: min_Y, maxY: max_Y,
+						minZ: min_Z, maxZ: max_Z };
+
+	}
+}
+
+module.exports = {
+	Item: Item
+}
---
+++ renderer/index.js
@@ -0,0 +1,184 @@
+'use strict'
+
+let radians = Math.PI/180
+let rotationX = 0
+let rotationZ = 0
+let pointAt = {
+  x: 1,
+  y: 1,
+  z: 0
+}
+let playerAt = {
+  x: 0,
+  y: 1,
+  z: 0
+}
+let cameraAt = {
+  x: 0,
+  y: 0,
+  z: 0
+}
+//let cameraView = 'firstPerson'
+let cameraView = 'firstPerson'
+let speed = 0.05
+let movementInput = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+let hudMode = false
+
+window.onmousemove = function (event) {
+  // Only move the camera if the player is not navigating the hud
+  if (!hudMode) {
+    let movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0
+    let movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0
+    rotationX -= movementX * -0.02
+    if ((rotationZ - movementY * 0.02) >= -90 && (rotationZ - movementY * 0.02) <= 90) {
+      rotationZ -= movementY * 0.02
+    }
+    rotationZ = Math.max(-90, Math.min(90, rotationZ))
+  }
+}
+window.onmousedown = function (event) {
+  document.body.requestPointerLock();
+}
+window.onkeydown = function (event) {
+          // Only move the player if the player is not navigating the hud
+  if (!hudMode) {
+    switch (event.key) {
+      case 'w':
+        movementInput.up = true
+        event.preventDefault()
+        break
+      case 's':
+        movementInput.down = true
+        event.preventDefault()
+        break
+      case 'a':
+        movementInput.left = true
+        event.preventDefault()
+        break
+      case 'd':
+        movementInput.right = true
+        event.preventDefault()
+        break
+      case 'ArrowUp':
+        event.preventDefault()
+        break
+      case 'ArrowDown':
+        event.preventDefault()
+        break
+      case 'ArrowLeft':
+        event.preventDefault()
+        break
+      case 'ArrowRight':
+        event.preventDefault()
+        break
+      case ' ':
+        event.preventDefault()
+        break
+
+      case 'h':
+        event.preventDefault()
+        if (UI.style.visibility == 'hidden') { UI.style.visibility = 'visible' } else { UI.style.visibility = 'hidden' }
+        break
+    }
+  } else {
+              // Enter
+    if (event.keyCode == 13 && document.activeElement == chatTextArea) {
+      event.preventDefault()
+    }
+  }
+}
+window.onkeyup = function (event) {
+  if (!hudMode) {
+    switch (event.key) {
+      case 'w':
+        movementInput.up = false
+        event.preventDefault()
+        break
+      case 's':
+        movementInput.down = false
+        event.preventDefault()
+        break
+      case 'a':
+        movementInput.left = false
+        event.preventDefault()
+        break
+      case 'd':
+        movementInput.right = false
+        event.preventDefault()
+        break
+    }
+  } else {
+    // Enter
+    if (event.keyCode == 13 && chatTextArea.value != '') {
+      // socket.emit('newChatMsg', chatTextArea.value);
+      chatTextArea.value = ''
+      event.preventDefault()
+    }
+  }
+  // Ctrl
+  if (event.keyCode == 17) {
+    // Turn HUD mode on/off
+    hudMode = !hudMode
+  }
+}
+
+function render (camera) {
+  if (movementInput.left) {
+    playerAt.x -= Math.cos((rotationX*radians) + Math.PI / 2) * speed
+    playerAt.z -= Math.sin((rotationX*radians) + Math.PI / 2) * speed
+  }
+  if (movementInput.right) {
+    playerAt.x += Math.cos((rotationX*radians) + Math.PI / 2) * speed
+    playerAt.z += Math.sin((rotationX*radians) + Math.PI / 2) * speed
+  }
+  if (movementInput.up) {
+    playerAt.x += Math.cos((rotationX*radians)) * speed
+    playerAt.z += Math.sin((rotationX*radians)) * speed
+  }
+  if (movementInput.down) {
+    playerAt.x -= Math.cos((rotationX*radians)) * speed
+    playerAt.z -= Math.sin((rotationX*radians)) * speed
+  }
+
+
+  if(cameraView == 'firstPerson')
+  {
+    pointAt.x = Math.cos(rotationX*radians) + playerAt.x
+    pointAt.y = Math.sin(rotationZ*radians) + playerAt.y
+    pointAt.z = Math.sin(rotationX*radians) + playerAt.z
+
+    camera.position.x = playerAt.x
+    camera.position.y = playerAt.y
+    camera.position.z = playerAt.z
+
+    camera.lookAt(pointAt)
+  }
+  else if(cameraView == 'thirdPerson')
+  {
+    pointAt.x = Math.cos(rotationX*radians)
+    pointAt.y = Math.sin(rotationZ*radians)
+    pointAt.z = Math.sin(rotationX*radians)
+
+    camera.position.x = -pointAt.x*2 + playerAt.x
+    camera.position.y = -pointAt.y*2 + playerAt.y
+    camera.position.z = -pointAt.z*2 + playerAt.z
+
+    camera.lookAt(playerAt)
+  }
+}
+
+function rotatePlayer(player) {
+  player.rotation.set(0, -rotationX*radians, 0)
+}
+
+module.exports = {
+  render: render,
+  rotatePlayer: rotatePlayer,
+  playerAt: playerAt,
+  pointAt: pointAt
+}
